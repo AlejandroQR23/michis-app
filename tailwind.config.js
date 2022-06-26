e089310835fafdf0f07c7ab1dca90b73a171b0a9
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2022-06-26 15:27:26 -0500

    created swing animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        swingInTop: {
+          '0%': {
+            transform: 'rotateX(-100deg)',
+            transformOrigin: 'top',
+            opacity: 0,
+          },
+          '100%': {
+            transform: 'rotateX(0deg)',
+            transformOrigin: 'top',
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.2s cubic-bezier(0.39, 0.575, 0.565, 1) both',
+        swingInTop: 'swingInTop 1s cubic-bezier(0.39, 0.575, 0.565, 1) both',
+      },
+    },
   },
   plugins: [],
 };
