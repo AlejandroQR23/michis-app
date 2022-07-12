523f7760255a1e95ddbb5909a6bef9cacd3fdc65
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2022-07-12 00:09:15 -0500

    added functionality to edit michis

--- src/pages/Homepage/homepage.component.tsx
+++ src/pages/Homepage/homepage.component.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react';
 import { Michi, michisAPI } from '../../api/michisAPI';
 import Loading from '../../components/Loading/loading.component';
 import MichiCard from '../../components/MichiCard/michi-card';
+import EditMichiModal from '../../components/Modal/edit-modal.component';
 
 const Homepage = () => {
   const [michis, setMichis] = useState<Michi[]>([]);
+  const [selectedMichi, setSelectedMichi] = useState<Michi | null>(null);
+  const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -20,7 +23,6 @@ const Homepage = () => {
         })
         .finally(() => {
           setLoading(false);
-          console.log('michis', michis);
         });
     })();
   }, []);
@@ -31,6 +33,18 @@ const Homepage = () => {
     });
   };
 
+  const handleSelectMichi = (michi: Michi) => {
+    setSelectedMichi(michi);
+    setShowModal(true);
+  };
+
+  const handleEditMichi = async (michi: Michi) => {
+    setShowModal(false);
+    await michisAPI.editMichi(michi).then(() => {
+      setMichis(michis.map((m) => (m._id === michi._id ? michi : m)));
+    });
+  };
+
   return (
     <div className="flex flex-row justify-center my-8">
       {loading ? (
@@ -38,8 +52,20 @@ const Homepage = () => {
       ) : (
         <div className="mx-10 flex flex-row flex-wrap gap-8 justify-center">
           {michis.map((michi) => (
-            <MichiCard key={michi._id} michi={michi} handleDelete={handleDelete} />
+            <MichiCard
+              key={michi._id}
+              michi={michi}
+              handleDelete={handleDelete}
+              handleSelectMichi={handleSelectMichi}
+            />
           ))}
+          {showModal && selectedMichi && (
+            <EditMichiModal
+              selectedMichi={selectedMichi}
+              handleEditMichi={handleEditMichi}
+              setShowModal={setShowModal}
+            />
+          )}
         </div>
       )}
     </div>
